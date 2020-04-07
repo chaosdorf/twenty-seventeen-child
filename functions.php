5cdac537a2ea7041e106652201b1fb6143db8a55
Niklas Sombert <niklas@ytvwld.de>
2020-04-07 20:35:17 +0200

    Only load the dark colors if needed

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
 add_action('wp_enqueue_scripts', 'twentyseventeen_child_scripts');
 function twentyseventeen_child_scripts() {
     wp_enqueue_style("parent-style", get_template_directory_uri() . "/style.css");
-    wp_enqueue_style("parent-style-dark", get_template_directory_uri() . "/assets/css/colors-dark.css");
+    wp_enqueue_style(
+        "parent-style-dark",
+        get_template_directory_uri() . "/assets/css/colors-dark.css",
+        array(), false, "(prefers-color-scheme: dark)"
+    );
     wp_enqueue_script("auto-dark",  get_theme_file_uri("auto-dark.js"), false);
 }
