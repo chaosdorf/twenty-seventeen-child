c4dd53df32630dfe548daf09865b9023e651ea9d
Niklas Sombert <niklas@ytvwld.de>
2020-04-07 18:42:39 +0200

    Fix file path

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
 add_action('wp_enqueue_scripts', 'twentyseventeen_child_scripts');
 function twentyseventeen_child_scripts() {
-    wp_enqueue_script("auto-dark", "auto-dark.js", false);
+    wp_enqueue_script("auto-dark",  get_theme_file_uri("auto-dark.js"), false);
 }
