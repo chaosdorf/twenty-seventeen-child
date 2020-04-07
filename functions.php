f1aefd57ab72febb221ea6444fe9c2669900514b
Niklas Sombert <niklas@ytvwld.de>
2020-04-07 18:44:37 +0200

    Don't drop the parent theme's css

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 add_action('wp_enqueue_scripts', 'twentyseventeen_child_scripts');
 function twentyseventeen_child_scripts() {
+    wp_enqueue_style("parent-style", get_template_directory_uri() . "/style.css");
     wp_enqueue_script("auto-dark",  get_theme_file_uri("auto-dark.js"), false);
 }
