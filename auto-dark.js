7bf30fbcb5bee93147aedcada75d1aa3339bb9f0
Niklas Sombert <niklas@ytvwld.de>
2020-04-07 20:42:51 +0200

    Don't use JavaScript to set the color scheme
    This has caused a flash on load.

--- auto-dark.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.getElementsByTagName("body")[0].classList.add("colors-dark");
-    }
-});
--- functions.php
+++ functions.php
@@ -1,11 +1,15 @@
 <?php
 add_action('wp_enqueue_scripts', 'twentyseventeen_child_scripts');
+// Just add the body class always.
+// We don't load the related CSS unless wanted.
+add_filter( 'body_class', function( $classes ) {
+    return array_merge( $classes, array( 'colors-dark' ) );
+} );
 function twentyseventeen_child_scripts() {
-    wp_enqueue_style("parent-style", get_template_directory_uri() . "/style.css");
+	wp_enqueue_style("parent-style", get_template_directory_uri() . "/style.css");
     wp_enqueue_style(
         "parent-style-dark",
         get_template_directory_uri() . "/assets/css/colors-dark.css",
         array(), false, "(prefers-color-scheme: dark)"
     );
-    wp_enqueue_script("auto-dark",  get_theme_file_uri("auto-dark.js"), false);
 }
