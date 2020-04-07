9418c8bedaa8caefbbd38f2fd0723e4799164227
Niklas Sombert <niklas@ytvwld.de>
2020-04-07 20:33:32 +0200

    Only modify the body when it's loaded

--- auto-dark.js
+++ auto-dark.js
@@ -1,3 +1,5 @@
-if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.getElementsByTagName("body")[0].classList.add("colors-dark");
-}
+window.addEventListener('DOMContentLoaded', (event) => {
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.getElementsByTagName("body")[0].classList.add("colors-dark");
+    }
+});
