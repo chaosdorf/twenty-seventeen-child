e9ea5b61463fdf35dbd48d13151bf4734db6afb2
Niklas Sombert <niklas@ytvwld.de>
2023-09-14 12:42:18 +0200

    static homepage

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The front page template file
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ */
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
+
+		This is static content.
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
